cb2ab7306c380ba88a5e4e78945e105ebb331a6f
Asraful Alam Azad <64921498+asrafulalamazad@users.noreply.github.com>
2023-09-18 00:52:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = require("./src/routes/api");
+const app = new express();
+const bodyParser = require("body-parser");
+
+//Security Middleware import
+const rateLimit= require('express-rate-limit');
+const helmet= require('helmet');
+const mongoSanitize= require('express-mongo-sanitize');
+const xss= require('xss-clean');
+const hpp= require('hpp');
+const cors= require('cors');
+
+//Database schema
+const mongoose =  require('mongoose');
+// const path = require("path");
+
+//Security Middleware implements
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+
+//image size
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb'}));
+
+//bodyParser implements
+app.use(bodyParser.json());
+
+// Rate Limiter implements
+const limiter = rateLimit({windowMs:15*60*100, max: 3000});
+app.use(limiter)
+//Database Connection
+
+let URI="mongodb+srv://<username>:<password>@cluster0.kxn7sfi.mongodb.net/BusinessTable?retryWrites=true&w=majority";
+let OPTION={user:'testuser',pass:'testuser',autoIndex:true};
+mongoose.connect(URI,OPTION,(error)=>{
+    console.log("Connection Success")
+    console.log(error)
+})
+
+
+
+// // managing Front End Tagging
+// app.use(express.static('client/build'))
+// app.get("*",function (req){
+//      req.sendFile(path.resolve(__dirname,'client','build','index.html'))
+// })
+
+
+
+//managing  back end api routing
+app.use("/api/v1", router);
+
+
+module.exports= app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/routes/api.js
@@ -0,0 +1,8 @@
+const  express = require("express");
+const ProductsController = require("../controllers/ProductsController");
+
+const router = express.Router();
+
+router.get("/ProductList/:pageNo/:perPage/:searchKeyword",ProductsController.ProductList );
+
+module.exports=router;
